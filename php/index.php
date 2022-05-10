74eda59a532e3b9c97f88220a47782c26e2491a9
Sriram R M <55828492+srirammurali99@users.noreply.github.com>
2022-05-10 12:05:01 +0530

    Update index.php

--- php/index.php
+++ php/index.php
@@ -5,12 +5,30 @@ echo 'Hello from Sriram R M<br/>';
 $host = 'db';
 $user = 'admin';
 $password = 'Adminpass19';
+$dbname = "myDB";
 
-$conn = mysqli_connect($host, $user, $password);
 
-if (!$conn) {
-	die('Cannot connect: ' . mysqli_connect_error());
+// Create connection
+$conn = new mysqli($host, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
-echo 'MYSQL connected successfully';
+
+// sql to create table
+$sql = "CREATE TABLE MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Table MyGuests created successfully";
+} else {
+  echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
 
 ?>
